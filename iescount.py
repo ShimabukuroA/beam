1fb1b29215496f7b440f15effe3d9e9ca6e800b0
Amauri Shimabukuro <amauri.shimabukuro@b2wdigital.com>
2019-04-30 17:14:05 -0300

    commit inicial

---
+++ iescount.py
@@ -0,0 +1,36 @@
+# Author: Amauri
+# Date: 29/04/2019
+#
+# Project to learn and develop data pipeline with Apache Beam and Python.
+
+from __future__ import print_function
+import apache_beam as beam
+
+
+# This function processes each row of .csv file and return a list with UF initials.
+def splitline(row):
+	parse = row.split(";")
+	uf = parse[9] #Sigla de UF
+	return (uf)
+
+# Create data pipeline.
+pipeline = beam.Pipeline()
+
+# Read each line from .csv file.
+lines = pipeline | beam.io.ReadFromText('./data/cadastro_ies.csv')
+
+# Get UF initials from each line .
+words = lines  | "split" >> beam.Map(splitline)
+
+# Mapping each UF initials in a tuple.
+maps = words	| "map" >> beam.Map(lambda row: (str(row),1))
+
+# Group by UF initials and count total number of educational institutions per UF.
+counts = maps    | "group" >> beam.CombinePerKey(sum)
+
+# Print result.
+counts |  "print" >> beam.Map(lambda row: print("%s : %d" %(row[0], row[1])))
+
+# Run the pipeline.
+pipeline.run()
+
