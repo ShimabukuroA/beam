f4864e1f1816b66b110a62f03773bfe7ec0c1241
Amauri Shimabukuro <amauri.shimabukuro@b2wdigital.com>
2019-05-02 15:30:36 -0300

    arrumado alguns problemas

--- iescount.py
+++ iescount.py
@@ -7,12 +7,18 @@ from __future__ import print_function
 import apache_beam as beam
 
 
+sortedlist = []
+
 # This function processes each row of .csv file and return a list with UF initials.
 def splitline(row):
 	parse = row.split(";")
 	uf = parse[9] #Sigla de UF
 	return (uf)
 
+# This functions appends each row from PCollection "Counts" in a list and sort by number of IES.
+def sortresults(row):
+	sortedlist.append(row) # Each row follows the format: (UF, number of IES)
+	sortedlist.sort(key=lambda x : x[1], reverse = True) # Sort list in decrescent order
 
 # Create data pipeline.
 pipeline = beam.Pipeline()
@@ -29,11 +35,12 @@ maps = words	| "map" >> beam.Map(lambda row: (row,1))
 # Group by UF initials and count total number of educational institutions per UF.
 counts = maps    | "group" >> beam.CombinePerKey(sum)
 
-result = counts	| "max" >> beam.FlatMap(lambda a,b: a if a[1] > b[1] else b)
+# Sorting results by number of IES. 
+counts	| "sort" >> beam.ParDo(sortresults)
 
 # Print result.
-result |  "print" >> beam.Map(lambda row: print("%s : %d" %(row[0], row[1])))
+#counts |  "print" >> beam.Map(lambda row: print("%s : %s" %(row[0], row[1])))
 
 # Run the pipeline.
 pipeline.run()
-
+print("UF com mais unidades de ensino superior:\n%s:%s unidades" % (sortedlist[0][0], sortedlist[0][1]))
